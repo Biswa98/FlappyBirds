66dad3480023c9d5036223bec777295308b3755a
Biswatosh Rout <routlikun@gmail.com>
2020-10-01 09:30:03 +0530

    New Commit with vhange in gitignore

---
+++ Flappy Bird/Assets/Scripts/BirdController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public float Speed = 2.0f;
+    public Rigidbody2D rb;
+    private Vector3 v = new Vector3(2, 3);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, Speed);
+        }
+    }
+}
